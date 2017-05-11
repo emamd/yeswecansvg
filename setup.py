f776d20184b6b3b2ac92d90e3c254eee4d82386a
Armand Emamdjomeh <armand.emamdjomeh@washpost.com>
2017-05-11 18:08:56 -0400

    starting git repo

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='yeswecansvg',
+    version='0.1.0',
+    description='Clean your SVGs so Illustrator won\'t tell you it CANT.',
+    author='Armand Emamdjomeh',
+    author_email='armand.emamdjomeh@washpost.com',
+    url='http://github.com/datadesk/latimes-calculate',
+    download_url='http://github.com/datadesk/latimes-calculate.git',
+    packages=("yeswecansvg",),
+    # cmdclass={'test': TestCommand},
+    # install_requires=('six>=1.4.1'),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
---
+++ yeswecansvg.py
@@ -0,0 +1,52 @@
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description="Convert an SVG's font declarations so that they open in Illustrator.")
+parser.add_argument('filename', help="Name of SVG file to be cleaned. Will output as filename_cleaned.svg")
+parser.add_argument('-f', '--font', default="\'FranklinITCStd-Light\'", help="Font to use for replacement. Defaults to 'FranklinITCStd-Light'.")
+parser.add_argument('-b', '--bold_font', default="\'FranklinITCStd-Bold\'", help="Bold font to use for replacement. Defaults to 'FranklinITCStd-Bold'.")
+
+args = parser.parse_args()
+
+
+def convert_svg_for_illustrator(filename, font="\'FranklinITCStd-Light\'", bold_font="\'FranklinITCStd-Bold\'"):
+    """
+    Convert all font definitions into something older versions of Illustrator will recognize and not crap out on.
+    Defaults to fonts we use at The Washington Post - Franklin Light and Franklin Bold.
+    Currently handles standard and bold fonts.
+    """
+    font_string = 'font-family:'
+    bold_string = 'font-weight:700'
+    bold_string_2 = 'font-weight:bold'
+
+    with open(filename, 'r+') as f:
+        outfilename = filename.replace(".svg", "")
+        outfilename = "%s_cleaned.svg" % outfilename
+
+        with open(outfilename, 'w+') as outfile:
+
+            for line in f:
+                isfont = re.search(font_string, line)
+                isbold = re.search(bold_string, line)
+
+                if isfont:
+                    if isbold:
+                        line = re.sub('<text', '<text font-family="%s"' % bold_font, line)
+                    else:
+                        line = re.sub('<text', '<text font-family="%s"' % font, line)
+
+                    # Remove font-family declaration
+                    line = re.sub('font-family:[A-Za-z \-]+;', '', line)
+                    # Remove font-weight declaration
+                    line = re.sub('font-weight:(\d+|\w+);', '', line)
+                    # Remove font-style declaration
+                    line = re.sub('font-style:\w+;', '', line)
+
+                outfile.write(line)
+
+            print("Exported cleaned file to %s" % outfilename)
+
+
+
+if __name__ == "__main__":
+    convert_svg_for_illustrator(args.filename, args.font, args.bold_font)
